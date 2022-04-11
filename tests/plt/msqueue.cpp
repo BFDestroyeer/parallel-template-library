3f5136592fef1043e48497de44ef376c5116dce6
Mikhail Zoreev <mzoreev@gmail.com>
2022-04-11 13:54:59 +0300

    msqueue

---
+++ tests/plt/msqueue.cpp
@@ -0,0 +1,14 @@
+#include <gtest/gtest.h>
+
+#include "ptl/msqueue"
+
+TEST(TEST_MSQUEUE, CONSTRUCTOR)
+{
+    ASSERT_NO_THROW(ptl::msqueue<int32_t> queue);
+}
+
+TEST(TEST_MSQUEUE, METHOD_PUSH)
+{
+    ptl::msqueue<int32_t> queue;
+    queue.push(10);
+}
