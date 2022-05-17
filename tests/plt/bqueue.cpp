8a4d51cb2bbe4c7412b795e5d8a1337bb7760dcf
BFDestroyeer <mzoreev@gmail.com>
2022-05-17 15:34:40 +0300

    basket queue initial

---
+++ tests/plt/bqueue.cpp
@@ -0,0 +1,15 @@
+#include <gtest/gtest.h>
+
+#include "ptl/bqueue"
+
+TEST(TEST_BQUEUE, CONSTRUCTOR)
+{
+    ASSERT_NO_THROW(ptl::bqueue<int32_t> queue);
+}
+
+TEST(TEST_BQUEUE, METHOD_PUSH)
+{
+    ptl::bqueue<int32_t> queue;
+    queue.push(10);
+    return;
+}
