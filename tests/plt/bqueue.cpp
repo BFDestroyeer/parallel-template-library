b2ed9aac7b8c0e50f1f9ccaef1cf067edfa84f06
BFDestroyeer <mzoreev@gmail.com>
2022-05-18 13:27:33 +0300

    msqueue destructor + benchmarks

--- tests/plt/bqueue.cpp
+++ tests/plt/bqueue.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include <omp.h>
 
 #include "ptl/bqueue"
 
@@ -19,4 +20,50 @@ TEST(TEST_BQUEUE, METHOD_POP)
     ptl::bqueue<int32_t> queue;
     queue.push(10);
     ASSERT_EQ(queue.pop(), 10);
+}
+
+TEST(BENCHMARK_BQUEUE, METHOD_PUSH)
+{
+    for (int j = 1; j <= 8; j++)
+    {
+        ptl::bqueue<int32_t> queue;
+        auto begin = omp_get_wtime();
+        omp_set_num_threads(j);
+#pragma omp parallel default(none), shared(j, queue, std::cout)
+        for (size_t i = 0; i < 100000; i++)
+        {
+            queue.push(0);
+        }
+        auto end = omp_get_wtime();
+        std::cout << "BENCHMARK_BQUEUE"
+                  << ":"
+                  << "METHOD_PUSH"
+                  << " " << "[" << j << "]"
+                  << " " << end - begin << std::endl;
+    }
+}
+
+TEST(BENCHMARK_BQUEUE, METHOD_POP)
+{
+    for (int j = 1; j <= 16; j++)
+    {
+        ptl::bqueue<int32_t> queue;
+        for (size_t i = 0; i < 100000; i++)
+        {
+            queue.push(0);
+        }
+        auto begin = omp_get_wtime();
+        omp_set_num_threads(j);
+#pragma omp parallel default(none), shared(j, queue, std::cout)
+        for (size_t i = 0; i < 100000; i++)
+        {
+            queue.pop();
+        }
+        auto end = omp_get_wtime();
+        std::cout << "BENCHMARK_BQUEUE"
+                  << ":"
+                  << "METHOD_POP"
+                  << " " << "[" << j << "]"
+                  << " " << end - begin << std::endl;
+    }
 }
--- tests/plt/msqueue.cpp
+++ tests/plt/msqueue.cpp
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include <omp.h>
 
 #include "ptl/msqueue"
 
@@ -18,4 +19,52 @@ TEST(TEST_MSQUEUE, METHOD_POP)
     ptl::msqueue<int32_t> queue;
     queue.push(10);
     queue.pop();
+}
+
+TEST(BENCHMARK_MSQUEUE, METHOD_PUSH)
+{
+    for (int j = 1; j <= 8; j++)
+    {
+        ptl::msqueue<int32_t> queue;
+        auto begin = omp_get_wtime();
+        omp_set_num_threads(j);
+#pragma omp parallel default(none), shared(j, queue, std::cout)
+        for (size_t i = 0; i < 100000; i++)
+        {
+            queue.push(0);
+        }
+        auto end = omp_get_wtime();
+        std::cout << "BENCHMARK_MSQUEUE"
+                  << ":"
+                  << "METHOD_PUSH"
+                  << " "
+                  << "[" << j << "]"
+                  << " " << end - begin << std::endl;
+    }
+}
+
+TEST(BENCHMARK_MSQUEUE, METHOD_POP)
+{
+    for (int j = 1; j <= 16; j++)
+    {
+        ptl::msqueue<int32_t> queue;
+        for (size_t i = 0; i < 100000; i++)
+        {
+            queue.push(0);
+        }
+        auto begin = omp_get_wtime();
+        omp_set_num_threads(j);
+#pragma omp parallel default(none), shared(j, queue, std::cout)
+        for (size_t i = 0; i < 100000; i++)
+        {
+            queue.pop();
+        }
+        auto end = omp_get_wtime();
+        std::cout << "BENCHMARK_MSQUEUE"
+                  << ":"
+                  << "METHOD_POP"
+                  << " "
+                  << "[" << j << "]"
+                  << " " << end - begin << std::endl;
+    }
 }
